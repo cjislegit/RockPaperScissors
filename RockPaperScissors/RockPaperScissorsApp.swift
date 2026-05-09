70de7e9b4e0674c8e38ce81518864adb6ead7c8e
cjislegit <carlosjramirez11@gmail.com>
2026-05-08 19:02:56 -0700

    Initial Commit

---
+++ RockPaperScissors/RockPaperScissorsApp.swift
@@ -0,0 +1,17 @@
+//
+//  RockPaperScissorsApp.swift
+//  RockPaperScissors
+//
+//  Created by Carlos Ramirez on 5/8/26.
+//
+
+import SwiftUI
+
+@main
+struct RockPaperScissorsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
