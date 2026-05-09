c2ab375becaae1871ad0ecf539e6f9195cfce757
cjislegit <carlosjramirez11@gmail.com>
2026-05-08 19:35:04 -0700

    Added a background gradient

--- RockPaperScissors/ContentView.swift
+++ RockPaperScissors/ContentView.swift
@@ -9,16 +9,24 @@ import SwiftUI
 
 struct ContentView: View {
     @State var currentChoice = ["rock", "paper", "scissors"].shuffled()
-    @State var win = Bool.random()
+    @State var shouldWin = Bool.random()
     
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        ZStack {
+            LinearGradient(colors: [
+                Color(red: 0.39, green: 0.42, blue: 0.18),
+                Color(red: 0.84, green: 0.77, blue: 0.06)
+            ], startPoint: .top, endPoint: .bottom)
+            
+            VStack {
+                Image(systemName: "globe")
+                    .imageScale(.large)
+                    .foregroundStyle(.tint)
+                Text("Hello, world!")
+            }
+            .padding()
         }
-        .padding()
+        .ignoresSafeArea()
     }
 }
 
