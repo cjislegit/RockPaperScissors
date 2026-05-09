ecfeab93acf376e79e8c09acf23f08c8adb2670c
cjislegit <carlosjramirez11@gmail.com>
2026-05-08 19:10:31 -0700

    Added state for current choice and win

--- RockPaperScissors/ContentView.swift
+++ RockPaperScissors/ContentView.swift
@@ -8,6 +8,9 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var currentChoice = ["rock", "paper", "scissors"].shuffled()
+    @State var win = Bool.random()
+    
     var body: some View {
         VStack {
             Image(systemName: "globe")
